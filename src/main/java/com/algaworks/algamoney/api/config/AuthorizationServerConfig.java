b13857b7cea36a28062501f31643cee4b2ffb2bb
luizpaulofranz <luizpaulofranz@gmail.com>
2018-01-02 22:06:39 -0200

    Adds Oauth2 authorization.

---
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,43 @@
+package com.algaworks.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+//essa classe eh responsavel pelas autenticacoes dos APLICATIVOS CLIENTES e não dos usuarios 
+//para criar um token, o spring cria um resource /oauth/token
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("angular")
+			.secret("@ngul@r0")
+			.scopes("read", "write")//esse escopo pode conter qlq array de strings
+				.authorizedGrantTypes("password").accessTokenValiditySeconds(1800);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+	}
+
+	@Bean
+	//para armazenar o token
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}	
+
+}
--- src/main/java/com/algaworks/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/ResourceServerConfig.java
@@ -1,35 +1,45 @@
 package com.algaworks.algamoney.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 //indica que e uma classe de configuracao
 @Configuration
 //habilita a seguranca na API
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableResourceServer
+//essa classe controla apenas os acessos aos recursos, dependendo da sua conexao
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password("admin").roles("ROLE");
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			//.antMatchers("/categorias").permitAll() //para permitir acesso sem autenticacao
 			.anyRequest().authenticated()
 			.and()
-			.httpBasic()//metodo de autenticacao
-			.and()
+			//.httpBasic().and()//metodo de autenticacao Basic
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//desabilita toda sessao
 			.and()
 			.csrf().disable();
+	}
+	
+	@Override
+	//pra tornar stateless
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception{
+		resources.stateless(true);
 	}
 
 }
