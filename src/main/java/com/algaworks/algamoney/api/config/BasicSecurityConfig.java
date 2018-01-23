652c279fb5ad981c648f349dba55dadc93cfb5ee
luizpaulofranz <luizpaulofranz@gmail.com>
2018-01-23 00:26:53 -0200

    Adds Pessoa filter by nome, using spring data JPA Query Method
    Adds a method in PessoaRepository, which filters the Pessoas by nome, the name of this ability is JPA Query Method.

--- src/main/java/com/algaworks/algamoney/api/config/BasicSecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algamoney.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Profile("basic-security")
+@Configuration
 @EnableWebSecurity
 /**
  * Essa classe faz a autenticacao basic, para uso em ambientes de teste.
