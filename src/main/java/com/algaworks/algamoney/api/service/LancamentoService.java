921bb786b0bcae7a57f63c8a757d9111653de427
luizpaulofranz <luizpaulofranz@gmail.com>
2018-04-24 23:12:51 -0300

    Adds Jasper Report, Lancamentos por Pessoa.

--- src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -1,9 +1,13 @@
 package com.algaworks.algamoney.api.service;
 
+import java.io.InputStream;
+import java.sql.Date;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
@@ -12,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.algamoney.api.dto.LancamentoCategoria;
 import com.algaworks.algamoney.api.dto.LancamentoDia;
+import com.algaworks.algamoney.api.dto.LancamentoPessoa;
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
@@ -19,6 +24,12 @@ import com.algaworks.algamoney.api.repository.PessoaRepository;
 import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
 import com.algaworks.algamoney.api.repository.projection.ResumoLancamento;
 import com.algaworks.algamoney.api.service.exception.PessoaInvalidaException;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class LancamentoService {
@@ -28,6 +39,23 @@ public class LancamentoService {
 
 	@Autowired
 	private PessoaRepository pessoas;
+	
+	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws JRException {
+		List<LancamentoPessoa> dados = repository.porPessoa(inicio, fim);
+		Map<String, Object> params = new HashMap<>();
+		// DT_INICIO foi definido no jasper reports
+		params.put("DT_INICIO", Date.valueOf(inicio));
+		params.put("DT_FIM", Date.valueOf(fim));
+		params.put("REPORT_LOCALE", new Locale("pt", "BR"));
+		// importamos o relatorio jasper
+		InputStream stream = this.getClass().getResourceAsStream("relatorios/lancamentos_pessoa.jasper");
+		// jasper esta no pom.xml
+		JasperPrint print = JasperFillManager.fillReport(
+				stream, params, 
+				new JRBeanCollectionDataSource(dados)
+			);
+		return JasperExportManager.exportReportToPdf(print);
+	}
 
 	/* Apply the filter */
 	public Page<Lancamento> list(LancamentoFilter filter, Pageable page) {
