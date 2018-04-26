ae50546412f62da91dfae4e0d2545fd6c784baaf
luizpaulofranz <luizpaulofranz@gmail.com>
2018-04-25 21:58:31 -0300

    Adds E-mail sendding configurations (using Spring properties).

--- src/main/java/com/algaworks/algamoney/api/config/AlgamoneyApiConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AlgamoneyApiConfig.java
@@ -1,39 +1,45 @@
 package com.algaworks.algamoney.api.config;
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * Essa classe permite configurarmos a aplicacao via arquivos properties, assim criamos nossas 
- * proprias propriedades de configuração.
+ * Essa classe permite configurarmos a aplicacao via arquivos properties, assim
+ * criamos nossas proprias propriedades de configuração.
  * 
- * Para funcionar, precisamos adicionar a anotação @ EnableConfigurationProperties no 
- * arquivo de bootstrap da aplicação: AlgamoneyApiApplication
+ * Para funcionar, precisamos adicionar a anotação @
+ * EnableConfigurationProperties no arquivo de bootstrap da aplicação:
+ * AlgamoneyApiApplication
  * 
- * O Arquivo que usamos essas configurações application-pro.properties.
- * As propriedades definidas nessa classe estão acessíveis nos arquivos de configuração, 
- * e podem ser utilizados na aplicação com os valores setados nos arquivos properties.
+ * O Arquivo que usamos essas configurações application-pro.properties. As
+ * propriedades definidas nessa classe estão acessíveis nos arquivos de
+ * configuração, e podem ser utilizados na aplicação com os valores setados nos
+ * arquivos properties.
  * 
  * Um exemplo de utilizacao dessa configuracao esta no resource TokenResource
- */ 
+ */
 
-//anotação necessaria para habilitar a classe como uma classe de configuração
-//algamoney é o nome dessas propriedades nos aquivos properties
+// anotação necessaria para habilitar a classe como uma classe de configuração
+// algamoney é o nome dessas propriedades nos aquivos properties
 @ConfigurationProperties("algamoney")
 @Component
 public class AlgamoneyApiConfig {
 
-	//configuração da URL atual do site
+	// configuração da URL atual do site
 	private String originPermitida = "http://localhost:8000";
 
-	//para tornar a classe Seguranca acessivel nos arquivos properties
+	// para tornar a classe Seguranca acessivel nos arquivos properties
 	private Seguranca seguranca = new Seguranca();
+	private Mail mail= new Mail();
 
 	public Seguranca getSeguranca() {
 		return seguranca;
 	}
 	
+	public Mail getMail() {
+		return mail;
+	}
+
 	public void setSeguranca(Seguranca s) {
 		this.seguranca = s;
 	}
@@ -47,10 +53,12 @@ public class AlgamoneyApiConfig {
 	}
 
 	/**
-	 *	Essa subclasse serve para podermos segmentar as configurações nos arquivos, assim podemos chamar
-	 *	nas configurações: algamoney.seguranca.enableHttps.
+	 * Essa subclasse serve para podermos segmentar as configurações nos
+	 * arquivos, assim podemos chamar nas configurações:
+	 * algamoney.seguranca.enableHttps.
 	 *
-	 *	O que faz o segurança ser acessivel, é a propriedade seguranca da classe wrapper.
+	 * O que faz o segurança ser acessivel, é a propriedade seguranca da classe
+	 * wrapper.
 	 */
 	public static class Seguranca {
 
@@ -64,6 +72,45 @@ public class AlgamoneyApiConfig {
 			this.enableHttps = enableHttps;
 		}
 
-}
-	
+	}
+
+	public static class Mail {
+		private String host;
+		private Integer port;
+		private String username;
+		private String password;
+
+		public void setHost(String host) {
+			this.host = host;
+		}
+		
+		public String getHost() {
+			return this.host;
+		}
+
+		public Integer getPort() {
+			return port;
+		}
+
+		public void setPort(Integer port) {
+			this.port = port;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	}
+
 }
