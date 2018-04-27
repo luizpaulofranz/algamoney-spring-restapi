8e004cd5b505f3946ea189c47b45d582c1789d23
luizpaulofranz <luizpaulofranz@gmail.com>
2018-04-26 22:57:28 -0300

    Adds Filter Methods Using Spring Data JPA (by methods names AMAZING).

--- src/main/java/com/algaworks/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/algaworks/algamoney/api/repository/LancamentoRepository.java
@@ -1,5 +1,8 @@
 package com.algaworks.algamoney.api.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
@@ -8,4 +11,7 @@ import com.algaworks.algamoney.api.repository.lancamento.LancamentoRepositoryQue
 //temos de declarar uma interface extender ela aqui, e criar uma classe para implementar ela
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery{
 
+	/** seguiundo esse padrao, o proprio JPA implementa a busca pra nos 
+	 * veja que as condicoes estao especificadas no nome do metodo*/
+	List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 }
